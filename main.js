2c2f62f3e688932686cd4b28f1ee494878ac6a91
Bruce M <84877032+brucem98@users.noreply.github.com>
2023-02-27 15:30:08 +0000

    install prompt package and edit print() method

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
@@ -5,27 +6,31 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    constructor(field) {
-      this._field = field;
-    }
-  
-    print() {
-      let printField = [];
-      for (let i = 0; i < this._field.length; i++) {
-        let arraytoString = this._field[i].toString();
-        printField.push(arraytoString);
-      }
-      let newString = printField.join('\n');
-      return newString.split(',').join('');
-    }    
-  
+  constructor(field = [[]]) {
+    this._field = field;
+    this._locationX = 0;
+    this._locationY = 0;
+    // this.field[0][0] = pathCharacter;
   }
-  
-  const myField = new Field([
-    ['*', '░', 'O'],
-    ['░', 'O', '░'],
-    ['░', '^', '░'],
-  ]);
-  
-  console.log(myField.print());
-  
+
+  print() {
+    console.log(this._field)
+    const displayString = this._field.map(x => {
+      return x.join('');
+    }).join('\n');
+    console.log(displayString);
+  }    
+
+
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+// const input = prompt('Which way? ');
+// console.log(`We are going ${input}`)
+
+console.log(myField.print());
+// console.log(myField.userInput(input))
