17a8d2935a3c5bc0b0cf92b52856589d3c3fb2ef
Bruce M <84877032+brucem98@users.noreply.github.com>
2023-02-28 16:18:56 +0000

    create isInBouds() method in Field class

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ class Field {
     this._field = field;
     this._locationX = 0;
     this._locationY = 0;
-    // this.field[0][0] = pathCharacter;
+    // Set the start position before the game starts
+    this._field[0][0] = pathCharacter;
   }
 
   print() {
@@ -19,22 +20,22 @@ class Field {
       return x.join('');
     }).join('\n');
     console.log(displayString);
-  }    
+  }
 
-  askQuestion() {
+   askQuestion() {
     const answer = prompt('Which way? ').toUpperCase();
     switch (answer) {
       case 'U':
-        this.locationY -= 1;
+        this._locationY -= 1;
         break;
       case 'D':
-        this.locationY += 1;
+        this._locationY += 1;
         break;
       case 'L':
-        this.locationX -= 1;
+        this._locationX -= 1;
         break;
       case 'R':
-        this.locationX += 1;
+        this._locationX += 1;
         break;
       default:
         console.log('Enter U, D, L or R.');
@@ -43,7 +44,19 @@ class Field {
     }
   }
 
-}
+  isInBounds() {
+    return (
+      this._locationY >= 0 &&
+      this._locationX >= 0 &&
+      this._locationY < this._field.length &&
+      this._locationX < this._field[0].length
+    );
+  }
+
+ 
+  }
+
+
 
 const myField = new Field([
   ['*', '░', 'O'],
@@ -55,3 +68,4 @@ const myField = new Field([
 
 console.log(myField.print());
 console.log(myField.askQuestion());
+console.log(myField.isInBounds());
